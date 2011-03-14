9e9d45aab8fc9e42964a322025c3e70988d1214d
David A. Hill <hill.david.a@gmail.com>
2011-03-14 17:44:33 -0500

    Fixing the php tags.

--- tasks.php
+++ tasks.php
@@ -1,1 +1,48 @@
-echo $_SERVER['REQUEST_URI'];
+<?php
+
+if($_POST['f'] == 'purge'){
+	$results['success'] = false;
+	
+	if(isset($_POST['target'])){
+		function deleteDirectory($dir) {
+			if(!file_exists($dir)) return true;
+			if(!is_dir($dir) || is_link($dir)) return unlink($dir);
+			foreach(scandir($dir) as $item){
+				if($item == '.' || $item == '..') continue;
+				if(!deleteDirectory($dir . "/" . $item)){
+					chmod($dir . "/" . $item, 0777);
+					if (!deleteDirectory($dir . "/" . $item)) return false;
+				};
+			}
+			return rmdir($dir);
+		}
+		
+		deleteDirectory($target);
+		mkdir($target);
+		
+		$purged = (sizeof(scandir($target)) == 2) ? true : false;
+		
+		if($purged){
+			$results['success'] = true;
+			$results['result'] = 'Purge successful.';
+		} else {
+			$results['result'] = 'Unable to purge, try again.';
+		}
+	}
+	
+	if($results['success'] == true){
+		echo header('HTTP/1.0 200 Ok');
+		echo json_encode( $results );
+	} else {
+		echo header('HTTP/1.0 400 Bad Request');
+		echo $results['result'];
+	}
+	
+} else {
+	$results['success'] = false;
+	$results['result'] = 'Invalid task.';
+	echo header('HTTP/1.0 400 Bad Request');
+	echo $results['result'];
+}
+
+?>
